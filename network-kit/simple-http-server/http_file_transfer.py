0b140c2f26a123589dbfaeb044ee41154d711f8f
meodaiduoi <meodaiduoi22@gmail.com>
2023-03-09 16:26:59 +0700

    fix capture filter

--- network-kit/simple-http-server/http_file_transfer.py
+++ network-kit/simple-http-server/http_file_transfer.py
@@ -8,125 +8,166 @@ from fastapi import Request, Response
 from fastapi import File, UploadFile
 from fastapi.responses import FileResponse
 
-import os, sys, subprocess, argparse
-import logging
+import os, sys, argparse
+import logging, tomli
 import datetime, time
-import threading
 
+import json
 import requests as rq
 
 app = FastAPI()
 
-class HostTask(BaseModel):
-    ip: str
-    port: int
-    filesize: int | None = None
-
-def generate_big_file(filename, size=100):
+# Utility functions
+def generate_big_file(filename, size=100) -> str:
     '''
         Create random file with name and
         size in unit MB
     '''
-    with open(f'{filename}', 'wb') as f:
+    file_path = f'temp/{filename}'
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    with open(f'{file_path}', 'wb') as f:
         f.seek(1024 * 1024 * size -1)
         f.write(str.encode("0"))
+    return file_path
 
-# Client
+def mkdir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+# API functions
+class FileTask(BaseModel):
+    ip: str
+    port: int
+    size: int | None = 100 # MByte
+
+class RemoveFileTask(BaseModel):
+    file_path: str
+
+@app.post('/remove_file/')
+async def remove_file(rm_task: RemoveFileTask):
+    file_path = rm_task.file_path
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
 @app.post('/upload_speed/')
-async def upload_speed(url, size=100):
+async def upload_speed(upload_task: FileTask):
     '''
         url with default upload size = 100mb
         filename id is unix time
         speed unit: Mbit/s
     '''
-    id_filename = str(int(time.time()*10**6))
-    filename = f'temp/{id_filename}'
-    generate_big_file(filename, size)
-    with open(f'{filename}', 'r') as upload_file:
+    filename = time.time_ns()
+    file_path = generate_big_file(filename, upload_task.size)
+    with open(f'{file_path}', 'r') as upload_file:
         start_time = time.time()
-        respone = rq.post(url, files={'file': upload_file})
+        respone = rq.post(f'http://{upload_task.ip}:{upload_task.port}/upload', 
+                          files={'file': upload_file})
         end_time = time.time()
-    os.remove(filename)
+    os.remove(file_path)
     
     speed = -1
     if respone.status_code == 200:
-        speed = size*8 / (end_time - start_time)
+        speed = upload_task.size*8 / (end_time - start_time)
         speed = round(speed, 5)
-    return {'upload_speed': speed}
+    # Upload speed is in Mbit/s
+    return Response(status_code=200, content={"upload_speed": speed})
+
+# @app.post("/upload")
+# async def upload(file: UploadFile = File(...)):
+    
+#     file_path = f'temp/{file.filename}'
+
+#     try:
+#         contents = await file.read()
+#         with open('test', 'wb') as f:
+#             f.write(file_path)
+#     except Exception:
+#         return {"message": "There was an error uploarding the file"}
+#     finally:
+#         await file.close()
+#         os.remove(file_path)
+#     return {"message": f"Successfuly uploaded {file.filename}"}
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
-    id_filename = str(int(time.time()*10**6))
-    file_path = f'temp/{id_filename}'
-
-    try:
+    file_path = f'temp/{file.filename}'
+    with open(file_path, 'wb') as f:
         contents = await file.read()
-        with open('test', 'wb') as f:
-            f.write(file_path)
-    except Exception:
-        return {"message": "There was an error uploarding the file"}
-    finally:
-        await file.close()
-    
-    task = threading.Thread(target=remove_file_after_download, args=(file_path, 1))
-    task.daemon = True
-    task.start()
-
+        f.write(contents)
     return {"message": f"Successfuly uploaded {file.filename}"}
 
+
 @app.post('/download_speed/')
-async def download_file(url, size=100):
+async def download_speed(download_task: FileTask):
     '''
         url with default download size = 100mb
         filename id is unix time
         speed unit: Mbit/s
     '''
-    id_filename = str(int(time.time()*10**6))
-    filename = f'temp/{id_filename}'
-    with rq.get(url, stream=True) as respone:
+    filename = time.time_ns()
+    file_path = f'temp/{filename}'
+    
+    with rq.get(f'http://{download_task.ip}:{download_task.port}/download/{download_task.size}', stream=True) as respone:
         start_time = time.time()
         respone.raise_for_status()
-        with open(filename, 'wb') as f:
+        with open(file_path, 'wb') as f:
             for chunk in respone.iter_content(chunk_size=8192): 
                 f.write(chunk)
         end_time = time.time()
         
         speed = -1
         if respone.status_code == 200:
-            size = os.path.getsize(filename)
+            size = os.path.getsize(file_path)
             speed = size/1024/1024*8 / (end_time - start_time)
             speed = round(speed, 5)
-        os.remove(filename)
-    return {'download_speed': speed}
+        os.remove(file_path)
+    rq.post(f'http://{download_task.ip}:{download_task.port}/remove_file', json=json.dumps({'file_path': filename}))
+    return Response(status_code=200, content={"download_speed": speed})
 
 @app.get('/download/{size}')
-async def download_file_with_size(size: int):
-    id_filename = str(int(time.time()*10**6))
-    file_path = f'temp/{id_filename}'
-    generate_big_file(file_path, size)
-    
-    task = threading.Thread(target=remove_file_after_download, args=(file_path,))
-    task.daemon = True
-    task.start()
-
+async def download(size: int=100):
+    filename = time.time_ns()
+    file_path = generate_big_file(filename, size)
     return FileResponse(path=file_path, filename=file_path); 
 
-
-@app.get('/respone_time')
-async def get_server_respone_time():
-    return {'time': int(time.time() * 1000)}
+class ResponeTimeTask(BaseModel):
+    ip: str
+    port: int | None = 8000
 
 @app.post('/respone_time')
-async def post_server_respone_time():
-    client_send_time = int (time.time() * 1000)
-    respone = rq.get('http://10.0.0.2:8000/respone_time')
-    server_recive_time = int(respone.json()['time'])
-    client_recive_time = int (time.time() * 1000)
-    rsp_time =  (server_recive_time - client_send_time) + (client_recive_time - server_recive_time) 
-    print(rsp_time)
-    return {'time': rsp_time} 
+async def server_respone_time(rpt_task: ResponeTimeTask):
+    '''
+        Step 1: client send request to server with time
+        Step 2: server recive request and send response with time
+        Step 3: client recive response and calculate time
+        return server time in milisecond
+    '''
+    cl_current_time = time.time_ns()
+    data = {
+        'cl_current_time': cl_current_time
+    }
+    sv_data = rq.post(f'http://{rpt_task.ip}:{rpt_task.port}/sv_respone_time', json=json.dumps(data)).json()
+    cl_to_sv = sv_data['cl_to_sv']
+    sv_current_time = sv_data['sv_current_time']
+    result_time = time.time_ns() - sv_current_time + cl_to_sv
+    
+    # return in milisecond
+    return Response(status_code=200, 
+                    content=json.dumps({"time": result_time/10**6}))
 
-if __name__ == '__main__':
+class ResponeTimeChainTask(BaseModel):
+    cl_current_time: int
+
+@app.post('/sv_respone_time')
+async def post_server_respone_time(rp_time: ResponeTimeChainTask):
+    sv_current_time = time.time_ns()
+    cl_to_sv_latency = time.time_ns() - rp_time.cl_current_time
+    return Response(status_code=200, 
+                    content=json.dumps({"cl_to_sv": cl_to_sv_latency,
+                                        'sv_current_time': sv_current_time}))
+
+def main():
     parser = argparse.ArgumentParser(description='simple cli client')
     
     parser.add_argument('ip', type=str,
@@ -136,11 +177,22 @@ if __name__ == '__main__':
     parser.add_argument('port', type=int,
                         default=8000,
                         help='port of server')
-
     args = parser.parse_args()
-    
+
+    # import config.toml
+    # try:
+    #     with open('config.toml', 'r') as f:
+    #         config = tomli.load(f)
+    #     mkdir(config['log_path'])
+    # except FileNotFoundError:
+    #     print('config.toml not found')
+    #     sys.exit(1)
+
+    # Create folder for temp file
+    mkdir('temp')
+    mkdir('log')
+
     # Create logger
-    if 
     file_handler = logging.FileHandler(
         filename=f'log/{args.ip}_{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}.log')
     stdout_handler = logging.StreamHandler(stream=sys.stdout)
@@ -154,3 +206,8 @@ if __name__ == '__main__':
 
     uvicorn.run(app, host=args.ip, port=args.port)
 
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
