a6f77d2b72d7154c8e175dd511ce65d76ecb800a
Thinh Ngo <lizadon321@gmail.com>
2023-03-13 17:51:52 +0700

    add gmail

---
+++ pcapcapture/pcapcapture/scenario/gmail.py
@@ -0,0 +1,55 @@
+# config and environment
+import tomli
+import sys, os
+import logging
+# import pandas as pd
+import time
+import random
+
+try:
+    with open('config.toml', 'rb') as f:
+        config = tomli.load(f)
+        interface = config['enviroment']['interface']
+        store_path = config['enviroment']['store_path']
+        profile_path = config['enviroment']['profile_path']
+        log_level = config['enviroment']['log_level']
+        # url_list = config['gmail']['url_list']
+
+        # To load module from parent folder
+        sys.path.insert(1, '../' )
+except FileNotFoundError:
+    logging.critical('Config file not found')
+    os._exit(1)
+
+# Code start from here
+from webcapture.pcapcapture import *
+from webcapture.ggservice import GmailPageLoader
+from webcapture.utils import *  
+
+if __name__ == '__main__':
+    try:
+        # Create folder to store output
+        pcapstore_path = os.path.join(mkpath_abs(store_path), 'QUIC', 'Gmail') 
+        sslkeylog_path = os.path.join(mkpath_abs(store_path), 'QUIC', 'Gmail', 'SSLKEYLOG')
+        mkdir_by_path(pcapstore_path)
+        mkdir_by_path(sslkeylog_path)
+
+        # Create logger
+        logging.basicConfig(filename=os.path.join(pcapstore_path, f'Gmail_{time.time_ns()}.log'), 
+                            level=log_level, format="%(asctime)s %(message)s")
+
+    
+
+    except KeyboardInterrupt:
+        gmail.close_driver()
+        capture.terminate()
+        capture.clean_up()
+        logging.error(f'Keyboard Interrupt at: {url} and {file_path}')
+        sys.exit(0)
+
+    except Exception as e:
+        gmail.close_driver()
+        capture.terminate()
+        capture.clean_up()
+        logging.critical(f'Error at: {url} and {file_path}')
+        raise e
