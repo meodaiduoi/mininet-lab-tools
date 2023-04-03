398f13adb71b20df7573334fcbdfabb7b5de64b1
Thinh Ngo <lizadon321@gmail.com>
2023-04-03 22:37:08 +0700

    Fix something

--- pcapcapture/pcapcapture/scenario/facebook.py
+++ pcapcapture/pcapcapture/scenario/facebook.py
@@ -1,9 +1,9 @@
 # config and environment
 import tomli
 import sys, os
+import logging
 import pandas as pd
 import time
-import logging
 import random
 
 try:
@@ -13,78 +13,64 @@ try:
         store_path = config['enviroment']['store_path']
         profile_path = config['enviroment']['profile_path']
         log_level = config['enviroment']['log_level']
-        url_list = config['tiktok']['url_list']
+        url_list = config['facebook']['url_list']
+
+        # To load module from parent folder
         sys.path.insert(1, '../' )
 except FileNotFoundError:
-    print('Config file not found')
-    exit(1)
+    logging.critical('Config file not found')
+    os._exit(1)
 
 # Code start from here
 from webcapture.pcapcapture import *
-from webcapture.social import TiktokLoader
-from webcapture.utils import *
-        
+from webcapture.social import Facebook
+from webcapture.utils import *        
+
 if __name__ == '__main__':
     try:
         # Create folder to store output
-        pcapstore_path = os.path.join(mkpath_abs(store_path), 'WEB', 'Tiktok')
-        sslkeylog_path = os.path.join(mkpath_abs(store_path), 'WEB', 'Tiktok', 'SSLKEYLOG')
+        pcapstore_path = os.path.join(mkpath_abs(store_path), 'WEB', 'Facebook') 
+        sslkeylog_path = os.path.join(mkpath_abs(store_path), 'WEB', 'Facebook', 'SSLKEYLOG')
         mkdir_by_path(pcapstore_path)
         mkdir_by_path(sslkeylog_path)
-        
-        # Create logger
-        file_handler = logging.FileHandler(filename=os.path.join(pcapstore_path, f'Tiktok_{time.time_ns()}.log'))
-        stdout_handler = logging.StreamHandler(stream=sys.stdout)
-        handlers = [file_handler, stdout_handler]
 
-        logging.basicConfig(
-            level=log_level, 
-            format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
-            handlers=handlers
-        )
+        # Create logger
+        logging.basicConfig(filename=os.path.join(pcapstore_path, f'Facebook_{time.time_ns()}.log'), 
+                            level=log_level, format="%(asctime)s %(message)s")
 
         # Load link from csv file
         df_link = pd.read_csv(url_list)
+
         for desc, url in zip(df_link['description'], df_link['url']):
 
+            capture = AsyncQUICTrafficCapture()
             filename = f'{desc}_{time.time_ns()}'
             file_path = os.path.join(pcapstore_path, filename)
             # Save ssl key to file
             os.environ['SSLKEYLOGFILE'] = os.path.join(sslkeylog_path, f'{filename}.log')
 
-            # Init driver and capture object
+            # Load facebook
             logging.info(f'Starting capture {url} to {file_path}')
-            tiktok = TiktokLoader(profile_path=profile_path)
-            capture = AsyncHTTPTrafficCapture()
+            facebook = Facebook(disable_cache=True, profile_path=profile_path)
 
-            # Load tiktok page and start capture
+            facebook.load(url)
             capture.capture(interface, f'{file_path}.pcap')
-            tiktok.load(url)
-            
-            # start_time = time.time()
-            # timer = 0
-    
-            tiktok.play()
-            # Load about 20-30 vid change url
-            for i in range(20):
-                tiktok.arrow_click('DOWN')
-                time.sleep(20)
 
-            # timer = time.time() - start_time
+            facebook.play()
 
             # Turn off capture and driver
             capture.terminate()
-            tiktok.close_driver()
+            facebook.close_driver()
 
     except KeyboardInterrupt:
-        tiktok.close_driver()
+        facebook.close_driver()
         capture.terminate()
         capture.clean_up()
         logging.error(f'Keyboard Interrupt at: {url} and {file_path}')
         sys.exit(0)
 
     except Exception as e:
-        tiktok.close_driver()
+        facebook.close_driver()
         capture.terminate()
         capture.clean_up()
         logging.critical(f'Error at: {url} and {file_path}')
