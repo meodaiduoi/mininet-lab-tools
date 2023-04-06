5fdea7ffcc73628d8d29e34adf5d098412be4660
meodaiduoi <meodaiduoi22@gmail.com>
2023-04-06 19:01:57 +0700

    adding tiktok

--- pcapcapture/pcapcapture/webcapture/social.py
+++ pcapcapture/pcapcapture/webcapture/social.py
@@ -7,6 +7,8 @@ from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.common.exceptions import (ElementNotInteractableException, ElementClickInterceptedException, 
                                         NoSuchElementException)
 
+from selenium.webdriver.common.keys import Keys
+
 import time
 import logging
 
@@ -19,29 +21,42 @@ class TiktokLoader(PageLoader):
                  url=None,
                  timeout=20,
                  profile_path: str = None,
-                 preferences=None,
-                 extensions=None,
+                 preferences: list[tuple[str, str]] = [],
+                 extensions: list[str] = [],
                  **kwargs):
         super(TiktokLoader, self).__init__(
-            (By.CLASS_NAME, "tiktok-xk7ai4-DivHeaderContainer e10win0d0"),
+            None,
             timeout, profile_path, preferences, extensions, **kwargs)
         self.start_driver()
         if url:
             self.load(url)
 
     def load(self, url):
+        # for future implementation
         if 'tiktok.com' in url:
-            super().load(url)
+            if 'video' in url:
+                super().load(url, (By.ID, 'main-content-video_detail'))
         else:
             logging.error('Not a valid tiktok url')
+            
+    def send_down_key(self):
+        self._driver.find_element(By.ID, 'main-content-video_detail').send_keys(Keys.DOWN)
 
-    def check_video(self):
+    @property
+    def captcha_block(self):
         try:
-            self._driver.find_element(
-                By.CLASS_NAME,
-                "tiktok-7tjqm6-DivBlurBackground e11s2kul8").get_attribute('style')
-        except AttributeError:
-            logging.error('Required to load() first')
+            if self._driver.find_element(By.CLASS_NAME, 'captcha_verify_container'):
+                return True
+        except NoSuchElementException:
+            return False
+
+    # def check_video(self):
+    #     try:
+    #         self._driver.find_element(
+    #             By.CLASS_NAME,
+    #             "tiktok-7tjqm6-DivBlurBackground e11s2kul8").get_attribute('style')
+    #     except AttributeError:
+    #         logging.error('Required to load() first')
 
 
 class FacebookVideo(PageLoader):
@@ -63,13 +78,14 @@ class FacebookVideo(PageLoader):
             self.load(url)
 
     def load(self, url):
-        if ('facebook.com' and 'watch') in url:
-            self.page_type = 'watch'
-            super().load(url, (By.CSS_SELECTOR, 'svg.x9f619'))
-        if ('facebook.com' and 'video') in url:
-            self.page_type = 'video'
-            super().load(url, (By.CSS_SELECTOR, "i.x14yjl9h > div:nth-child(1) > i:nth-child(1)"))
-            self._video_play()
+        if 'facebook.com' in url:
+            if 'watch' in url:
+                self.page_type = 'watch'
+                super().load(url, (By.CSS_SELECTOR, 'svg.x9f619'))
+            if 'video' in url:
+                self.page_type = 'video'
+                super().load(url, (By.CSS_SELECTOR, "i.x14yjl9h > div:nth-child(1) > i:nth-child(1)"))
+                self._video_play()
         else:
             logging.error('Not a valid facebook url')
 
